47b57b3f9bf314cb859349855699eaa2a7065370
AlexandreDChaves <xandechaves@gmail.com>
2025-02-24 10:49:12 -0300

    configurando bootstrap & criando a pasta partials include

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.use(express.static('public'))
 app.get("/", (req,res) => {
     res.render("index")   
 })
+app.get("/perguntar", (req,res) => {
+    res.render("perguntar")   
+})
 
 app.listen(3000,() => {
     console.log("servidor ok")
